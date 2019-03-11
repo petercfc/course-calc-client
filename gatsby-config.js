0a3e1608d77f86fd624b5b6c467290e21dcc2bad
Peter Arnold <peter@cagefreeclimbing.com>
2019-03-11 12:53:04 -0400

    pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,32 @@
 module.exports = {
   plugins: [
     {
-      resolve: 'gatsby-plugin-material-ui',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        pathToTheme: 'src/utils/theme.js',
-      },
+        name: "Course Credit Calc",
+        short_name: "CCC",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`
+      }
     },
+    `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-material-ui",
+      options: {
+        pathToTheme: "src/utils/theme.js"
+      }
+    }
   ],
   siteMetadata: {
-    title: 'Gatsby Material-UI next',
-  },
+    title: "Course Credit Calculator"
+  }
 };
