dc2113e7a60d306d52fa923eec3e45659acc8d43
Peter Arnold <peter@cagefreeclimbing.com>
2019-03-16 17:13:40 -0400

    working delete

--- src/components/students.js
+++ src/components/students.js
@@ -3,13 +3,13 @@ import React from "react";
 
 // apollo
 import gql from "graphql-tag";
-import { Query } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
 
 // material-ui
 import withRoot from "../withRoot";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import selectStudent from "./selectStudent";
+import Button from "@material-ui/core/Button";
 
 // create material-ui styles init with theme
 const styles = theme => ({
@@ -25,6 +25,14 @@ const GET_ALL_STUDENTS = gql`
   }
 `;
 
+const DELETE_STUDENT = gql`
+  mutation deleteStudent($id: ID!) {
+    deleteStudent(where: { id: $id }) {
+      id
+    }
+  }
+`;
+
 // main class
 class Students extends React.Component {
   // render main
@@ -32,21 +40,34 @@ class Students extends React.Component {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <Query query={GET_ALL_STUDENTS}>
-          {({ loading, error, data }) => {
-            if (loading) return "Loading...";
-            if (error) return `Error! ${error.message}`;
-            return (
-              <div>
-                {data.students.map(student => (
-                  <Typography key={student.id} variant="body1" gutterBottom>
-                    {student.name} - {student.id}
-                  </Typography>
-                ))}
-              </div>
-            );
-          }}
-        </Query>
+        <Mutation mutation={DELETE_STUDENT}>
+          {(deleteStudent, { data }) => (
+            <Query query={GET_ALL_STUDENTS}>
+              {({ loading, error, data }) => {
+                if (loading) return "Loading...";
+                if (error) return `Error! ${error.message}`;
+                return (
+                  <div>
+                    {data.students.map(student => (
+                      <Typography key={student.id} variant="body1" gutterBottom>
+                        {student.name} - {student.id}{" "}
+                        <Button
+                          onClick={() =>
+                            deleteStudent({
+                              variables: { id: student.id }
+                            })
+                          }
+                        >
+                          Delete
+                        </Button>
+                      </Typography>
+                    ))}
+                  </div>
+                );
+              }}
+            </Query>
+          )}
+        </Mutation>
       </div>
     );
   }
