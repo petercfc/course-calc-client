e6570d54b70f8b52fa214a0ba9e1337610ef7a46
Peter Arnold <peter@cagefreeclimbing.com>
2019-03-18 10:44:57 -0400

    ssr and code splitting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import withRoot from "../withRoot";
-import Courses from "./courses";
-import Calculator from "./calculator";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Loadable from 'react-loadable';
+
+const Courses = Loadable({
+  loader: () => import("./courses"),
+  loading: () => <div>loading...</div>,
+});
+
+const Calculator = Loadable({
+  loader: () => import("./calculator"),
+  loading: () => <div>loading...</div>,
+});
+
 
 const styles = theme => ({
   root: {}
